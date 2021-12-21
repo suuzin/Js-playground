e597472ed4e699e3b5ea86a155813f760fabe408
suuzin <8oozin@gmail.com>
2021-12-22 01:56:11 +0900

    첫번째 순서확인

--- wordRelay/script.js
+++ wordRelay/script.js
@@ -10,3 +10,16 @@ const getNum = Number(prompt('몇명이 참가하나요?'));
 const onInput = (event) => {
     newWord = event.target.value;
 }
+
+const onClickBtn = () => {
+    if(!word){
+        word = newWord;
+        getWord.textContent = word;
+        order = Number(getOrder.textContent);
+    }else {
+        alert('제대로 입력해주세요');
+    }
+}
+
+getBtn.addEventListener("click",onClickBtn);
+getInput.addEventListener("input",onInput);
