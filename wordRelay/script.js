ef5c2c679a093f5a5703b0a1767b705b2232be5d
suuzin <8oozin@gmail.com>
2021-12-22 01:54:30 +0900

    참가인원 저장 변수 지정

--- wordRelay/script.js
+++ wordRelay/script.js
@@ -2,3 +2,8 @@ const getBtn = document.querySelector('button');
 const getInput = document.querySelector('input');
 const getWord = document.querySelector("#word");
 const getOrder = document.querySelector("#order");
+let word;
+let newWord;
+let order;
+
+const getNum = Number(prompt('몇명이 참가하나요?'));
