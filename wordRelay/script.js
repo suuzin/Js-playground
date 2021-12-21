911b0b806cb27ac92fb8f2f149373fa7595d4e1d
suuzin <8oozin@gmail.com>
2021-12-22 01:55:03 +0900

    input 내용 저장

--- wordRelay/script.js
+++ wordRelay/script.js
@@ -5,5 +5,8 @@ const getOrder = document.querySelector("#order");
 let word;
 let newWord;
 let order;
-
 const getNum = Number(prompt('몇명이 참가하나요?'));
+
+const onInput = (event) => {
+    newWord = event.target.value;
+}
