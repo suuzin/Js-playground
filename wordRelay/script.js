69f9a18609749f824d0577af95627ba76c952a4e
suuzin <8oozin@gmail.com>
2021-12-23 02:07:36 +0900

    함수 좀 더 나눠서 구현

--- wordRelay/script.js
+++ wordRelay/script.js
@@ -1,40 +1,43 @@
-const getBtn = document.querySelector('button');
-const getInput = document.querySelector('input');
-const getWord = document.querySelector("#word");
-const getOrder = document.querySelector("#order");
-let word;
+const getNum = Number(prompt('참여인원수를 입력해주세요.')); //참여인원수
+const turnNum = document.querySelector(".turnNum");
+const ruleWord = document.querySelector(".ruleWord");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+let keyword;
 let newWord;
-let order;
-const getNum = Number(prompt('몇명이 참가하나요?'));
 
-const onInput = (event) => {
-    newWord = event.target.value;
-}
-
-const onClickBtn = () => {
-    if(!word || word[word.length -1] === newWord[0]){
-        word = newWord;
-        getWord.textContent = word;
-        order = Number(getOrder.textContent);
-        checkIndex(order);
-    }else {
-        alert('제대로 입력해주세요');
-    }
-    getReset();
-}
-
-const checkIndex = (order) => {
+function checkTurn(){
+    let order = Number(turnNum.textContent);
     if(order + 1 > getNum){
-        getOrder.textContent =1;
-    }else {
-        getOrder.textContent = order +1 ;
+        turnNum.textContent = 1;
+    }else{
+        turnNum.textContent = order + 1;
     }
 }
 
-const getReset = () => {
-    getInput.value = "";
-    getInput.focus();
+function checkRule(){
+    if(!keyword || keyword[keyword.length-1] === newWord[0]){
+        keyword = newWord;
+        ruleWord.textContent = keyword;
+    }else{
+        alert("다시 입력해주세요");
+    }
 }
 
-getBtn.addEventListener("click",onClickBtn);
-getInput.addEventListener("input",onInput);
+function onInput(e){
+    newWord = e.target.value;
+}
+
+function onClick(){
+    checkTurn();
+    checkRule();
+    reset();
+}
+
+function reset(){
+    input.value = "";
+    input.focus();
+}
+
+button.addEventListener("click",onClick);
+input.addEventListener("input",onInput);
