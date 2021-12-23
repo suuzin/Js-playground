dfbc250a4b93445eaa083f98ed2a171c07bb8a68
suuzin <8oozin@gmail.com>
2021-12-24 01:17:51 +0900

    클릭하면 실행하는 함수 추가

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,3 +1,17 @@
+let getResult = document.querySelector("#result");
+let clickedNum = [];
+
+let onClickNumber = (event) => {
+    let str = event.target.textContent;
+    console.log(str);
+
+    if(str === '+'||str === '-'||str ==='*'||str === '/'){
+        clickedNum.push(' ' + str + ' ');
+    }else{
+        clickedNum.push(str);
+    }
+};
+
 document.querySelector('#num-0').addEventListener('click', onClickNumber);
 document.querySelector('#num-1').addEventListener('click', onClickNumber);
 document.querySelector('#num-2').addEventListener('click', onClickNumber);
