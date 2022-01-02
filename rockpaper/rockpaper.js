788ad923e8a52b0ee8098c38b6e27ec1b7be7145
suuzin <8oozin@gmail.com>
2022-01-02 22:57:41 +0900

    js파일 생성 및 html 태그 가져오기

---
+++ rockpaper/rockpaper.js
@@ -0,0 +1,5 @@
+const getComputer = document.querySelector("#computer");
+const getScore = document.querySelector("#score");
+const getRock = document.querySelector("#rock");
+const getScissors = document.querySelector("#scissors");
+const getPaper = document.querySelector("#paper");
